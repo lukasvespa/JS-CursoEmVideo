84ad2058c6bf1a51a039a546ef3ad3991cdcf3e8
Lucas Vespa <lucasvesparangel@gmail.com>
2023-07-04 01:15:15 -0300

    Create ex008.js

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 75
+console.log(`Sua velocidade é de ${vel}km/h`);
+
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade, será MULTADO!`);
+}
+
+console.log(`Dirija com cuidado!`);
